06f4ebe1a3438216d03ad7f2b78f8b107b1fbf9f
00ZRHun <00zrhun@gmail.com>
2022-05-30 19:11:12 +0800

    Dependency Injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,10 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+    @Autowired  // dependency injection
     public StudentController(StudentService studentService) {
-        this.studentService = studentService;
+        // this.studentService = new StudentService();  // Work but X good, so use dependency injection
+         this.studentService = studentService;  // dependency injection
     }
 
     @GetMapping
