4025e838044e592ec65e7604b95775547ae39ae9
00ZRHun <00zrhun@gmail.com>
2022-05-31 11:06:53 +0800

    JPA and @Entity

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity  // for hibernate
+@Table   // for tbl in db
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     // Variables
     private Long id;
     private String name;
